8ceaa469f745117ffbdedf4840cc95e20390df5f
Yasha Burns <yasha@wds4u.com>
2024-05-05 16:54:22 -0500

    Variable jump, coyote time, jump buffer, speed acceleration/decceleration

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var audio_stream_player = $AudioStreamPlayer2D
 
 func _on_body_entered(body):
 	print("You died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
+	audio_stream_player.play()
 
 
 func _on_timer_timeout():
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 125.0
-const JUMP_VELOCITY = -300.0
+@export var maxSpeed = 125.0
+@export var jumpVelocity = -300.0
+@export var acceleration : int = 20
+@export var decceleration : float = 0.2
+@export var jumpBufferTime : int = 5
+@export var coyoteTime : int = 5
+
+var jumpBufferCounter : int = 0
+var coyoteCounter : int = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,13 +17,30 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity and start coyoteCounter
 	if not is_on_floor():
+		coyoteCounter -= 1
 		velocity.y += gravity * delta
-
+		
+	# Coyote counter
+	if is_on_floor():
+		coyoteCounter = coyoteTime
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		jumpBufferCounter = jumpBufferTime
+	
+	if jumpBufferCounter > 0:
+		jumpBufferCounter -= 1
+	
+	if jumpBufferCounter > 0 and coyoteCounter > 0:
+		velocity.y = jumpVelocity
+		jumpBufferCounter = 0
+		coyoteCounter = 0
+	
+	# Variable Jump Height
+	if velocity.y < 0 and Input.is_action_just_released("jump"):
+		velocity.y = 0
 
 	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -37,9 +61,14 @@ func _physics_process(delta):
 		animated_sprite.play("jump")
 	
 	# Movement
-	if direction:
-		velocity.x = direction * SPEED
+	if direction == 1:
+		velocity.x += acceleration
+	elif direction == -1:
+		velocity.x -= acceleration 
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#	velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = lerp(velocity.x,float(0),decceleration)
+	
+	velocity.x = clamp(velocity.x, -maxSpeed, maxSpeed)
 
 	move_and_slide()
