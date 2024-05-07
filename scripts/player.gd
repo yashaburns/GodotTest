d79e081210e9c1fb7c130d4bb9be9a059818ec69
Yasha Burns <yasha@wds4u.com>
2024-05-06 19:11:38 -0500

    finally fixed dash

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,12 +7,14 @@ extends CharacterBody2D
 @export var acceleration : int = 20
 @export var decceleration : float = 0.2
 @export var jumpBufferTime : int = 5
-@export var coyoteTime : int = 5
+@export var coyoteTime : int = 10
 @export var dashPower : int = 300
 @export var dashDuration : int = 15
 @export var dashCooldown : int = 60
-@export var ghost_node : PackedScene
 
+
+var lastDirection : int = -1
+var flippedGhost : bool = false
 var dashing : bool = false
 var dashDurTimer : int = 0
 var dashTimer : int = 0
@@ -22,11 +24,13 @@ var coyoteCounter : int = 0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var dash_effect = $DashEffect
+@onready var ghost = $Ghost
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var trail = $Trail
-@onready var ghost_time = $ghostTime
 
 func _physics_process(delta):
+	var img = dash_effect.texture.get_image()
+	
 	# Add the gravity and start coyoteCounter
 	if not is_on_floor():
 		coyoteCounter -= 1
@@ -66,13 +70,15 @@ func _physics_process(delta):
 	
 	# Flip sprite
 	if direction > 0:
+		flippedGhost = true
 		animated_sprite.flip_h = true
 	elif direction < 0:
+		flippedGhost = false
 		animated_sprite.flip_h = false
 		
 	# Play anims
 	if is_on_floor():
-		if direction == 0:
+		if direction == 0 and direction > -0.3 and direction < 0.3:
 			animated_sprite.play("newIdle")
 		else:
 			animated_sprite.play("newRun")
@@ -85,13 +91,17 @@ func _physics_process(delta):
 		dashTimer = dashCooldown
 		velocity.x = dashPower * direction
 		dashDurTimer = dashDuration
+		if direction != lastDirection:
+			img.flip_x()
+			dash_effect.texture = ImageTexture.create_from_image(img)
+			lastDirection = direction
 	elif dashDurTimer == 0:
 		dashing = false
-	
+		
 	# Movement
-	if direction > 0.1:
+	if direction > 0.3:
 		velocity.x += acceleration
-	elif direction < -0.1:
+	elif direction < -0.3:
 		velocity.x -= acceleration 
 	else:
 	#	velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -99,18 +109,9 @@ func _physics_process(delta):
 		
 	if dashing == false:
 		velocity.x = clamp(velocity.x, -maxSpeed, maxSpeed)
-		trail.visible = false
-		trail.clear_points()
+		dash_effect.emitting = false
 	if dashing == true:
-		trail.visible = true
+		dash_effect.emitting = true
 		velocity.x = clamp(velocity.x, -maxSpeedWhileDashing, maxSpeedWhileDashing)
 
 	move_and_slide()
-	
-func add_ghost():
-	var ghost = ghost_node.instantiate()
-	ghost.set_property(position, $AnimatedSprite2D.scale)
-	get_tree().current_scene.add_child(ghost)
-
-func _on_ghost_time_timeout():
-	add_ghost()
